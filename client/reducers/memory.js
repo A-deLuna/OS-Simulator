11654b95e8c432ad14d9aa36bf774f1a6e291722
Antonio de Luna <tonydelun@gmail.com>
2015-12-10 11:45:57 -0800

    fix'

--- client/reducers/memory.js
+++ client/reducers/memory.js
@@ -2,21 +2,23 @@ import { createReducer } from 'utils';
 import * as MemoryConstants from '../constants/Memory';
 
 const initialState = {
-  table: new Array(256 / 4)
+  table: new Array(256 / 4),
+  frameSize: 4,
+  ram: 256
 };
 
 for (let i = 0; i < (initialState.ram / initialState.frameSize) / 4; i++) {
-  initialState.table[i] = MemoryConstants.OS;
+  initialState.table[i] = {val: MemoryConstants.OS, time: Number.MAX_SAFE_INTEGER};
 }
 for (let i =  (initialState.ram / initialState.frameSize) / 4; i < initialState.table.length; i++) {
-  initialState.table[i] = MemoryConstants.EMPTY;
+  initialState.table[i] = {val: MemoryConstants.EMPTY, time: 0};
 }
 
 export default createReducer(initialState, {
   [MemoryConstants.SET_MEMORY] : (state, payload) => {
     alert(JSON.stringify(payload));
     return Object.assign({}, state, {
-      table: [...state.table.slice(0, payload.index), payload.id, ...state.table.slice(payload.index + 1)]
+      table: [...state.table.slice(0, payload.index), {val: payload.id, time: payload.time }, ...state.table.slice(payload.index + 1)]
     });
   }
 });
